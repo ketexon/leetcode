56274be2c644c83484f1195e82ed43e95f02d1f1
Aubrey Clark (ketexon) <zane.a.s.clark@gmail.com>
2026-06-23 21:57:08 -0500

    Made 3699 faster

--- array/3699-number-of-zigzag-arrays-i.cpp
+++ array/3699-number-of-zigzag-arrays-i.cpp
@@ -8,29 +8,24 @@ constexpr int mod = 1000000007;
 
 class Solution {
 public:
-    // to simplify: we will consider all zigZags starting up and multiply by 2
     int zigZagArrays(int n, int l, int r) {
         int m = (r - l) + 1;
-        // M[len - 1][last_num] = number of zigzags of length n
-        vector<vector<int>> M(n, vector<int>(m, 0));
-
-        // 1 way for zig zag of 1 element
-        for (int i = 0; i < m; ++i) {
-            M[0][i] = 1;
-        }
+        vector<int> M(m, 1);
+        vector<int> NextM(m, 0);
 
         for (int i = 1; i < n; ++i) {
             bool up = i % 2 == 0;
             int sum = 0;
             for (int last_num = up ? 0 : m - 1; last_num < m && last_num >= 0; last_num += (up ? 1 : -1)) {
-                M[i][last_num] = sum;
-                sum = (sum + M[i-1][last_num]) % mod;
+                NextM[last_num] = sum;
+                sum = (sum + M[last_num]) % mod;
             }
+            std::swap(M, NextM);
         }
 
         int result = 0;
         for (int last_num = 0; last_num < m; ++last_num) {
-            result = (result + M[n-1][last_num]) % mod;
+            result = (result + M[last_num]) % mod;
         }
         
         return (result * 2) % mod;
